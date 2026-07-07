6aea5360bc24309b361e226d9818ce327e2d9130
Trần Gia Huy <tonytran0314@gmail.com>
2026-07-07 01:21:44 -0400

    layout for payments

--- src/components/Payments.vue
+++ src/components/Payments.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-    import { PlusCircle } from '@lucide/vue';
+    import { Ellipsis, PlusCircle } from '@lucide/vue';
 </script>
 
 <template>
@@ -7,49 +7,38 @@
         <!-- Header -->
         <div class="flex items-center justify-between">
             <div class="text-2xl font-semibold">Payments</div>
-            <div class="bg-indigo-100 flex items-center cursor-pointer gap-1.5 rounded-md px-4 py-3 text-indigo-600">
+            <div class="bg-indigo-100 flex items-center cursor-pointer gap-1.5 rounded-md px-4 py-3 text-indigo-600 hover:bg-indigo-600 hover:text-white transition duration-150">
                 <div><PlusCircle class="size-5" /></div>
                 <div class="text-sm font-semibold">New payment</div>
             </div>
         </div>
-        <!-- Content -->
-        <!-- <div class="bg-violet-400 h-[4000px]">content here</div> -->
-        <div>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis quis qui, veritatis sapiente nulla aperiam est impedit, rerum quo rem fugit. Nostrum commodi molestias explicabo sint. Voluptatem tenetur ab error?</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque cupiditate aperiam odit ipsa quisquam repellendus veritatis, eligendi saepe sed explicabo architecto ab tempora sunt, minima illo facilis laudantium voluptas officiis.</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque cupiditate aperiam odit ipsa quisquam repellendus veritatis, eligendi saepe sed explicabo architecto ab tempora sunt, minima illo facilis laudantium voluptas officiis.</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque cupiditate aperiam odit ipsa quisquam repellendus veritatis, eligendi saepe sed explicabo architecto ab tempora sunt, minima illo facilis laudantium voluptas officiis.</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque cupiditate aperiam odit ipsa quisquam repellendus veritatis, eligendi saepe sed explicabo architecto ab tempora sunt, minima illo facilis laudantium voluptas officiis.</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque cupiditate aperiam odit ipsa quisquam repellendus veritatis, eligendi saepe sed explicabo architecto ab tempora sunt, minima illo facilis laudantium voluptas officiis.</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque cupiditate aperiam odit ipsa quisquam repellendus veritatis, eligendi saepe sed explicabo architecto ab tempora sunt, minima illo facilis laudantium voluptas officiis.</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque cupiditate aperiam odit ipsa quisquam repellendus veritatis, eligendi saepe sed explicabo architecto ab tempora sunt, minima illo facilis laudantium voluptas officiis.</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque cupiditate aperiam odit ipsa quisquam repellendus veritatis, eligendi saepe sed explicabo architecto ab tempora sunt, minima illo facilis laudantium voluptas officiis.</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque cupiditate aperiam odit ipsa quisquam repellendus veritatis, eligendi saepe sed explicabo architecto ab tempora sunt, minima illo facilis laudantium voluptas officiis.</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque cupiditate aperiam odit ipsa quisquam repellendus veritatis, eligendi saepe sed explicabo architecto ab tempora sunt, minima illo facilis laudantium voluptas officiis.</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque cupiditate aperiam odit ipsa quisquam repellendus veritatis, eligendi saepe sed explicabo architecto ab tempora sunt, minima illo facilis laudantium voluptas officiis.</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque cupiditate aperiam odit ipsa quisquam repellendus veritatis, eligendi saepe sed explicabo architecto ab tempora sunt, minima illo facilis laudantium voluptas officiis.</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque cupiditate aperiam odit ipsa quisquam repellendus veritatis, eligendi saepe sed explicabo architecto ab tempora sunt, minima illo facilis laudantium voluptas officiis.</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque cupiditate aperiam odit ipsa quisquam repellendus veritatis, eligendi saepe sed explicabo architecto ab tempora sunt, minima illo facilis laudantium voluptas officiis.</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque cupiditate aperiam odit ipsa quisquam repellendus veritatis, eligendi saepe sed explicabo architecto ab tempora sunt, minima illo facilis laudantium voluptas officiis.</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque cupiditate aperiam odit ipsa quisquam repellendus veritatis, eligendi saepe sed explicabo architecto ab tempora sunt, minima illo facilis laudantium voluptas officiis.</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque cupiditate aperiam odit ipsa quisquam repellendus veritatis, eligendi saepe sed explicabo architecto ab tempora sunt, minima illo facilis laudantium voluptas officiis.</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque cupiditate aperiam odit ipsa quisquam repellendus veritatis, eligendi saepe sed explicabo architecto ab tempora sunt, minima illo facilis laudantium voluptas officiis.</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque cupiditate aperiam odit ipsa quisquam repellendus veritatis, eligendi saepe sed explicabo architecto ab tempora sunt, minima illo facilis laudantium voluptas officiis.</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque cupiditate aperiam odit ipsa quisquam repellendus veritatis, eligendi saepe sed explicabo architecto ab tempora sunt, minima illo facilis laudantium voluptas officiis.</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque cupiditate aperiam odit ipsa quisquam repellendus veritatis, eligendi saepe sed explicabo architecto ab tempora sunt, minima illo facilis laudantium voluptas officiis.</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque cupiditate aperiam odit ipsa quisquam repellendus veritatis, eligendi saepe sed explicabo architecto ab tempora sunt, minima illo facilis laudantium voluptas officiis.</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque cupiditate aperiam odit ipsa quisquam repellendus veritatis, eligendi saepe sed explicabo architecto ab tempora sunt, minima illo facilis laudantium voluptas officiis.</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque cupiditate aperiam odit ipsa quisquam repellendus veritatis, eligendi saepe sed explicabo architecto ab tempora sunt, minima illo facilis laudantium voluptas officiis.</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque cupiditate aperiam odit ipsa quisquam repellendus veritatis, eligendi saepe sed explicabo architecto ab tempora sunt, minima illo facilis laudantium voluptas officiis.</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque cupiditate aperiam odit ipsa quisquam repellendus veritatis, eligendi saepe sed explicabo architecto ab tempora sunt, minima illo facilis laudantium voluptas officiis.</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque cupiditate aperiam odit ipsa quisquam repellendus veritatis, eligendi saepe sed explicabo architecto ab tempora sunt, minima illo facilis laudantium voluptas officiis.</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque cupiditate aperiam odit ipsa quisquam repellendus veritatis, eligendi saepe sed explicabo architecto ab tempora sunt, minima illo facilis laudantium voluptas officiis.</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque cupiditate aperiam odit ipsa quisquam repellendus veritatis, eligendi saepe sed explicabo architecto ab tempora sunt, minima illo facilis laudantium voluptas officiis.</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque cupiditate aperiam odit ipsa quisquam repellendus veritatis, eligendi saepe sed explicabo architecto ab tempora sunt, minima illo facilis laudantium voluptas officiis.</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque cupiditate aperiam odit ipsa quisquam repellendus veritatis, eligendi saepe sed explicabo architecto ab tempora sunt, minima illo facilis laudantium voluptas officiis.</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque cupiditate aperiam odit ipsa quisquam repellendus veritatis, eligendi saepe sed explicabo architecto ab tempora sunt, minima illo facilis laudantium voluptas officiis.</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque cupiditate aperiam odit ipsa quisquam repellendus veritatis, eligendi saepe sed explicabo architecto ab tempora sunt, minima illo facilis laudantium voluptas officiis.</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque cupiditate aperiam odit ipsa quisquam repellendus veritatis, eligendi saepe sed explicabo architecto ab tempora sunt, minima illo facilis laudantium voluptas officiis.</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque cupiditate aperiam odit ipsa quisquam repellendus veritatis, eligendi saepe sed explicabo architecto ab tempora sunt, minima illo facilis laudantium voluptas officiis.</div>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque cupiditate aperiam odit ipsa quisquam repellendus veritatis, eligendi saepe sed explicabo architecto ab tempora sunt, minima illo facilis laudantium voluptas officiis.</div>
+        <!-- Filter -->
+        <!-- <div class="bg-violet-300 h-32 shrink-0">filter here</div> -->
+        
+        <div class="grid grid-cols-2 gap-4">
+
+            <div v-for="value in 9" class="border border-solid rounded-lg p-4 border-zinc-200 flex gap-3">
+                <div class="bg-zinc-300 flex-4 h-48 flex items-center justify-center text-zinc-400 rounded-lg"><div>image</div></div>
+                <div class="flex-3 flex flex-col justify-between">
+                    <div class="flex flex-col gap-1">
+                        <div class="text-xl">Netflix</div>
+                        
+                        <div class="text-md">$0/month</div> 
+
+                        <div class="line-clamp-1 text-zinc-400 text-sm">On 2nd and 15th</div>
+                        <div class="line-clamp-1 text-zinc-400 text-sm">From Jul 3, 2026</div>
+                        <div class="line-clamp-1 text-zinc-400 text-sm">To Jul 3, 2027</div>
+                    </div>
+                    <!-- <div class="flex justify-center items-center gap-4">
+                        <div class="bg-indigo-100 text-indigo-600 cursor-pointer hover:bg-indigo-600 hover:text-white py-2 rounded-md transition duration-150 w-full text-center text-sm">Add money</div>
+                        <div><Ellipsis class="size-5 cursor-pointer" /></div>
+                    </div> -->
+                </div>
+            </div>
+            
+        </div>
+
+        <!-- pagination -->
+        <!-- <div>pagination</div> -->
     </div>
 </template>
