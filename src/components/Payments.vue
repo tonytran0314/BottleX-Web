358d87bc17406dc56e0f0bb4cc1f8fb2486bc5c1
Trần Gia Huy <tonytran0314@gmail.com>
2026-07-10 09:03:40 -0400

    load data for payments

--- src/components/Payments.vue
+++ src/components/Payments.vue
@@ -1,5 +1,30 @@
 <script setup lang="ts">
-    import { Ellipsis, PlusCircle } from '@lucide/vue';
+import { PlusCircle } from '@lucide/vue';
+import api from '../services/axios';
+import { ref } from 'vue';
+
+type Payment = {
+    id: number;
+    name: string;
+    amount: number;
+    startDate: string;
+    endDate: string;
+    userId: number;
+    dueDates: number[]; // fix the type
+}
+
+const payments = ref<Payment[] | null>(null);
+const isLoadingPayments = ref(true);
+
+async function getPayments() {
+    const response = await api.get("/payments");
+console.log(response.data)
+    isLoadingPayments.value = false;
+
+    payments.value = response.data;
+}
+
+getPayments();
 </script>
 
 <template>
@@ -17,17 +42,17 @@
         
         <div class="grid grid-cols-2 gap-4">
 
-            <div v-for="value in 9" class="border border-solid rounded-lg p-4 border-zinc-200 flex gap-3">
+            <div v-for="payment in payments" class="border border-solid rounded-lg p-4 border-zinc-200 flex gap-3">
                 <div class="bg-zinc-300 flex-4 h-48 flex items-center justify-center text-zinc-400 rounded-lg"><div>image</div></div>
                 <div class="flex-3 flex flex-col justify-between">
                     <div class="flex flex-col gap-1">
-                        <div class="text-xl">Netflix</div>
+                        <div class="text-xl">{{ payment.name }}</div>
                         
-                        <div class="text-md">$0/month</div> 
+                        <div class="text-md">${{ payment.amount }}</div> 
 
-                        <div class="line-clamp-1 text-zinc-400 text-sm">On 2nd and 15th</div>
-                        <div class="line-clamp-1 text-zinc-400 text-sm">From Jul 3, 2026</div>
-                        <div class="line-clamp-1 text-zinc-400 text-sm">To Jul 3, 2027</div>
+                        <div v-for="dueDate in payment.dueDates" class="line-clamp-1 text-zinc-400 text-sm">On {{ dueDate.dueDateOnMonth }}</div>
+                        <div class="line-clamp-1 text-zinc-400 text-sm">From {{ payment.startDate }}</div>
+                        <div class="line-clamp-1 text-zinc-400 text-sm">To {{ payment.endDate }}</div>
                     </div>
                     <!-- <div class="flex justify-center items-center gap-4">
                         <div class="bg-indigo-100 text-indigo-600 cursor-pointer hover:bg-indigo-600 hover:text-white py-2 rounded-md transition duration-150 w-full text-center text-sm">Add money</div>
